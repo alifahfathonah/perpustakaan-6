bc03a054b0e8e176abe1c7619e9948481a327299
Dio Dava Ramadha <dioramadhan74@gmail.com>
2020-06-13 14:48:25 +0700

    update

--- application/controllers/Judul.php
+++ application/controllers/Judul.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Judul extends Operator_controller {
+class Judul extends Operator_Controller {
 
 
 	public function __construct() {
